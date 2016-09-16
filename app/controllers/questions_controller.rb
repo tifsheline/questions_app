06e375cc718fc0dbd4fb97c72f73b1f2c42d305c
Tiffany Glover <tif.sheline@gmail.com>
2016-09-16 12:42:15 -0700

    Initial Commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to(root_path)
+  end
+end
